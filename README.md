56c0c156106d6f0e008ac274f0fd0430dd8c25b1
danieltien13 <danieltien13@gmail.com>
2023-11-07 06:38:22 -0800

    update code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vinyl_recommender
 Uses Spotify API to recommend a vinyl to purchase based on your history
+
+Can be run using `python vinyl_recommender.py`
--- secrets_template.py
+++ secrets_template.py
@@ -1,4 +1,5 @@
 # Confidential client information for Spotify, don't upload your actual token to GitHub!
 # Instead, copy this file and rename it to `actual_secrets.py` and paste your information in that file
+
+# To get your token, you can run one of the scripts at https://developer.spotify.com/ and copy it over
 spotify_token = ""
-spotify_user_id = ""
